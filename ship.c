7e853986846cd1ff1c08c5bda8d8de1c07572d9a
Corey Hines <cbh344@outlook.com>
2024-10-14 21:05:58 +1300

    added documentation for ship

--- ship.c
+++ ship.c
@@ -1,7 +1,21 @@
+/** @file   ship.c
+ *  @author Corey Hines, Ethan Field
+ *  @date   14 October 2024
+ */
+
 #include <stdint.h>
 #include <stdbool.h>
 #include "ship.h"
 
+/**
+ * @brief Calculates the bottom coordinates of a ship based on its orientation and type.
+ * @param orientation The orientation of the ship (horizontal or vertical).
+ * @param ship_type The type of the ship, representing its length.
+ * @param x_top The x-coordinate of the top of the ship.
+ * @param y_top The y-coordinate of the top of the ship.
+ * @param x_bottom A pointer to store the calculated x-coordinate of the bottom of the ship.
+ * @param y_bottom A pointer to store the calculated y-coordinate of the bottom of the ship.
+ */
 static void calculate_bottom_coords(
     Orientation_t orientation, 
     ShipType_t ship_type,
@@ -22,6 +36,14 @@ static void calculate_bottom_coords(
     }
 }
 
+/**
+ * @brief Creates a ship object with the specified parameters.
+ * @param orientation The orientation of the ship (horizontal or vertical).
+ * @param length The length/type of the ship.
+ * @param x_top The x-coordinate of the top of the ship.
+ * @param y_top The y-coordinate of the top of the ship.
+ * @return A Ship_t object with its attributes initialized.
+ */
 Ship_t ship_create(Orientation_t orientation, ShipType_t length, uint8_t x_top, uint8_t y_top)
 {
     uint8_t x_bottom, y_bottom;
--- ship.h
+++ ship.h
@@ -1,13 +1,28 @@
+/** @file   ship.h
+ *  @author Corey Hines, Ethan Field
+ *  @date   14 October 2024
+ *  @brief   This file defines the structures and functions for 
+ *           handling ship creation and manipulation.
+ */
+
 #include <stdint.h>
 #include <stdbool.h>
 
-#define ORIENTATION_HORIZONTAL 0
-#define ORIENTATION_VERTICAL 1
-#define SMALL_SHIP_LENGTH 2
-#define LARGE_SHIP_LENGTH 3
+#define ORIENTATION_HORIZONTAL 0  /** Defines a horizontal ship orientation. */
+#define ORIENTATION_VERTICAL 1    /** Defines a vertical ship orientation. */
+#define SMALL_SHIP_LENGTH 2       /** Defines the length of a small ship. */
+#define LARGE_SHIP_LENGTH 3       /** Defines the length of a large ship. */
 
+/**
+ * @typedef Orientation_t
+ * @brief   Type definition for representing ship orientation.
+ */
 typedef uint8_t Orientation_t;
 
+/**
+ * @struct Ship_t
+ * @brief  Represents a ship with its attributes.
+ */
 typedef struct
 {
     Orientation_t orientation;
@@ -19,10 +34,22 @@ typedef struct
     bool sunk;
 } Ship_t;
 
+/**
+ * @enum  ShipType_t
+ * @brief Enumeration for ship types.
+ */
 typedef enum 
 {
     SMALL = SMALL_SHIP_LENGTH,
     LARGE = LARGE_SHIP_LENGTH
 } ShipType_t;
 
+/**
+ * @brief Creates a ship object with the specified parameters.
+ * @param orientation The orientation of the ship (horizontal or vertical).
+ * @param length The length/type of the ship.
+ * @param x_top The x-coordinate of the top of the ship.
+ * @param y_top The y-coordinate of the top of the ship.
+ * @return A Ship_t object with its attributes initialized.
+ */
 Ship_t ship_create(Orientation_t orientation, ShipType_t length, uint8_t x_top,  uint8_t y_top);
